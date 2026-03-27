8dfa86cb8558e6a45bf5c6795d5eb5be2d1cf091
elton santos <eltonsantos.es.51@gmail.com>
2026-03-27 15:16:27 -0300

    explorando uma nova função.py

--- projeto git github/helo_mundo.py
+++ projeto git github/helo_mundo.py
@@ -10,4 +10,16 @@ def soma (a,b):
 
 print(soma(5,8))
 
-print('nova ramificação')
+def soma_valores_diferentes(n1,n2,n3):
+    if n1 == n2 == n3:
+        return 0
+    if n1 == n2 != n3:
+        return n3
+    if n1 == n3 != n2:
+        return n2
+    if n2 == n3 != n1:
+        return n1
+    else:
+        return n1+n2+n3
+    
+print(soma_valores_diferentes(5,15,10))
